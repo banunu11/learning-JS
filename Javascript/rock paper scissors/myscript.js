39b1e3cca335d2cdd6c1363624d6cf25967fcc12
banunu11 <banunu1011@gmail.com>
2024-03-21 10:06:08 +0800

    finished rock paper scissors program

--- Javascript/rock paper scissors/myscript.js
+++ Javascript/rock paper scissors/myscript.js
@@ -1,6 +1,65 @@
 
+function Computerchoice(){
+    let choices = [`Rock`, `Paper`, `Scissors`];
+    num = Math.floor(Math.random() * choices.length);
+    if (num == 1){
+        return `Rock`;
+    }
+    else if (num == 2){
+        return `Paper`
+    }
+    else {
+        return `Scissors`
+    }
+}
+
+function Playerinput(select){
+    player = select.charAt(0).toUpperCase() + select.slice(1).toLowerCase();
+    return player;
+}
+
+let playerselect = prompt("Choose an action: ");
+playerselect = Playerinput(playerselect);
+let computerselect = Computerchoice();
+
+function choose(player, computer){
+    if (player == 'Rock'){
+        if (computer == 'Rock'){    
+            return `It's a tie!`
+        }
+        else if (computer == 'Paper'){
+            return 'You lose! Paper beats Rock!'
+        }
+        else{
+            return 'You win! Rock beats Scissors!'
+        }
+    }
+    if (player == 'Paper'){
+        if (computer == 'Paper'){    
+            return `It's a tie!`
+        }
+        else if (computer == 'Rock'){
+            return 'You win! Paper beats Rock!'
+        }
+        else{
+            return 'You lose! Scissors beats Paper!'
+        }
+    }
+    else {
+        if (computer == 'Scissors'){    
+            return `It's a tie!`
+        }
+        else if (computer == 'Rock'){
+            return 'You lose! Rock beats Scissors!'
+        }
+        else{
+            return 'You win! Scissors beats Paper!'
+        }
+    }
+}
 
 
+console.log(choose(playerselect, computerselect))
 
 
 
